a155e4f5b3c4ba8e6485fbb033e71314d6a414aa
ant30 <ant30tx@gmail.com>
2014-01-09 10:55:58 +0100

    Upgrade the gecoscc-ui version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ant30tx@gmail.com'
 license          'EUPL V 1.1 http://www.osor.eu/eupl'
 description      'Installs/Configures gecoscc-ui'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2'
 
 depends 'python'
 depends 'build-essential'
