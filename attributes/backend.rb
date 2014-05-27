adda8f6a4c1dbc0ce7c0804fbba0ea2592333297
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-05-27 11:48:07 +0200

    Updated tag version and some parameterized

--- attributes/backend.rb
+++ attributes/backend.rb
@@ -9,8 +9,10 @@
 # http://www.osor.eu/eupl
 #
 
-default['gecoscc-ui']['backend']['package'] = 'https://github.com/gecos-team/gecoscc-ui/archive/0.4.5.tar.gz'
-default['gecoscc-ui']['backend']['version'] = '0.4.5'
+default['gecoscc-ui']['backend']['package'] = 'https://github.com/gecos-team/gecoscc-ui/archive/0.5.0.tar.gz'
+default['gecoscc-ui']['backend']['version'] = '0.5.0'
 default['gecoscc-ui']['backend']['virtual_prefix'] = '/opt/gecosccui-'
 default['gecoscc-ui']['backend']['workers'] = 2
 default['gecoscc-ui']['backend']['firewall'] = 'lokkit'
+default['gecoscc-ui']['chef-server']['url'] = 'https://localhost/'
+default['gecoscc-ui']['mongodb']['url'] = 'mongodb://localhost:27017/gecoscc'
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'gecosccui'
-maintainer       'Antonio Perez-Aranda Alcaide'
-maintainer_email 'ant30tx@gmail.com'
+maintainer       'Yaco Sistemas S.L.'
+maintainer_email 'sysadmin@yaco.es'
 license          'EUPL V 1.1 http://www.osor.eu/eupl'
 description      'Installs/Configures gecoscc-ui'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2'
+version          '0.5'
 
 depends 'python'
 depends 'build-essential'
